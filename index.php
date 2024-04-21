81bb118a9cd1f0e8055ed3f915f01ec3f4e7058c
Tilak-21 <tilakamlesh2112@gmail.com>
2024-04-21 02:38:27 -0400

    Added summary page and result page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 //This is my CONTROLLER!
 
@@ -23,11 +24,44 @@ $f3->route('GET /', function() {
 });
 
 //Order route
-$f3->route('GET /order', function() {
+$f3->route('GET|POST /order', function($f3) {
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+    if(empty($POST['pet'])) {
+        echo "Please specify a Pet Type!";
+    }
+//    creating variables and using post
+    $petname = $_POST['pet'];
+    $petcolor = $_POST['color'];
+
+
+
+
+//    adding to session array.
+    $f3-> set('SESSION.petname', $petname);
+    $f3-> set('SESSION.petcolor', $petcolor);
+
+//    Redirection after successful submission.
+    $f3->reroute("summary");
+
+}
+//    else {
+//        echo "GET Method!";
+//    }
 
 
     $view = new Template();
     echo $view->render('views/pet-order.html');
+
+});
+
+$f3->route('GET /summary', function() {
+//    echo '<h1>Dinner is served!</h1>';
+
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
 
 });
 
